42900fba9c2d0a7c6df1d4e46dc53aacbe55d256
MrFunfantastic <brendancinelli@hotmail.com>
2016-07-13 15:07:59 -0400

    Test App

---
+++ App/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+	private bool grounded;
+	private bool shooting;
+	private Animator anim;
+	private Rigidbody2D rigid;
+
+	
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+		rigid = GetComponent<Rigidbody2D> ();
+
+	}	
+
+	void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.W) && grounded)
+		{
+			Jump ();
+		}
+	
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			MoveRight ();
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			MoveLeft ();
+
+		}
+
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			shooting = true;
+
+		}
+
+		anim.SetFloat ("Speed", Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x));
+		anim.SetFloat ("Jump", Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.y));
+		anim.SetBool ("Grounded", grounded);
+		anim.SetBool ("Shoot", shooting);
+	}
+
+	public void MoveRight()
+	{
+		FaceDirection(Vector2.right);
+		rigid.velocity = new Vector2 (moveSpeed, rigid.velocity.y);
+	}
+
+	public void MoveLeft()
+	{
+		FaceDirection(Vector2.left);
+		rigid.velocity = new Vector2 (-moveSpeed, rigid.velocity.y);
+	}
+
+	public void Shoot()
+	{
+		
+	}
+
+	public void Jump()
+	{
+		if(grounded)
+			rigid.velocity = new Vector2 (0, jumpHeight);
+	}
+
+	private void FaceDirection(Vector2 direction)
+	{
+		Quaternion rotation3D = direction == Vector2.right ? Quaternion.LookRotation(Vector3.forward) : Quaternion.LookRotation(Vector3.back); 
+		transform.rotation = rotation3D;
+	}
+
+
+}
---
+++ App/Assets/Scripts/TouchControls.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControls : MonoBehaviour {
+
+	private PlayerController player;
+
+	private bool right;
+	private bool left;
+	private bool jump;
+
+	// Use this for initialization
+	void Start () {
+	
+		player = FindObjectOfType<PlayerController> ();
+		right = false;
+		left = false;
+		jump = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//RIGHT ARROW
+		if (right) {
+			RightArrow ();
+		} 			
+		//LEFT ARROW
+		else if (left) {
+			LeftArrow ();
+		} 
+
+	}
+
+	//RIGHT BUTTON
+	public void onPointerDownRight()
+	{
+		right = true;	
+	}
+	public void onPointerUpRight()
+	{
+		right = false;	
+	}
+
+	//LEFT BUTTON
+	public void onPointerDownLeft()
+	{
+		left = true;	
+	}
+	public void onPointerUpLeft()
+	{
+		left = false;	
+	}
+
+	//JUMP
+	public void onPointerDownJump()
+	{
+		jump = true;	
+	}
+	public void onPointerUpJump()
+	{
+		jump = false;	
+	}
+
+	public void LeftArrow()
+	{
+		player.MoveLeft ();
+	}
+
+	public void RightArrow()
+	{
+		player.MoveRight ();
+	}
+
+	public void UnpressedArrow()
+	{
+		player.Unpress ();
+	}
+
+	public void Jump()
+	{
+		player.Jump ();
+	}
+
+	public void Shoot()
+	{
+
+	}
+
+
+
+}
